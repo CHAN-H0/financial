44bdfacde049cb542d89f9e7aa7e08e48a427cfe
LIM <lch0309k@gmail.com>
2024-02-08 16:11:04 +0900

    스케쥴러

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -4,11 +4,15 @@ import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        System.out.println("Main -> " + Thread.currentThread().getName());
     }
 }
